1f2d72489a8299065d0ff75e13b4f1b7e6cf2f91
Michał Zieliński <michal@zielinscy.org.pl>
2018-11-22 12:15:24 +0100

    remove examples

--- example/component.mk
+++
@@ -1,4 +0,0 @@
-COMPONENT_SRCDIRS:=.
-CXXFLAGS=-std=c++11 -Wall -Wno-sign-compare -mlongcalls
-COMPONENT_OBJS:=main.o
-COMPONENT_ADD_INCLUDEDIRS := .
--- example/default_wifi_config.h
+++
@@ -1,4 +0,0 @@
-
-const char* DEFAULT_SSID = "yyy";
-const char* DEFAULT_PASS = "xxx";
-const char* HNET_IP = "zzz";
